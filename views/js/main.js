d7040632ee101dc6371b8b8d2477183711f66bfe
wcalvert88 <wcalvert88@gmail.com>
2018-03-01 11:14:51 -0500

    setup the basics

---
+++ views/js/main.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function () {
+    $(".resultsDelete").click(function () {
+      console.log("this", $(this));
+      console.log("this parent parent", $(this).parent().parent());
+      var parentDiv = $(this).parent().parent().parent();
+      console.log("parentDiv", parentDiv);
+      console.log("parentDiv.span stuff", parentDiv.find("span").text());
+      eventId = parentDiv.find("span").text();
+      console.log(eventId);
+      eventId = eventId.trim().split("").join("");
+      console.log("new EventId", eventId);
+      $.ajax({
+        type: "DELETE",
+        url: "/searchEvents.ejs",
+        data: {
+          calendarID: 'primary',
+          eventId: eventId,
+          showNotifications: false
+        },
+        success: function (msg) {
+          console.log("I did it!");
+        }
+      });
+      // cal.Events.delete('primary', parentDiv.find("span").text(), {sendNotifications: false})
+      //   .then(results => {
+      //     console.log('delete Event:' + JSON.stringify(results));
+      //   }).catch(err => {
+      //     console.log('Error deleteEvent:' + JSON.stringify(err.message));
+      //   });
+    })
+  });
