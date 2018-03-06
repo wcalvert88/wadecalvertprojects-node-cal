78b5cbb710b9e0101450d088b72b43ba78364087
wcalvert88 <wcalvert88@gmail.com>
2018-03-05 19:13:42 -0500

    I think I've completed it except for cleaning up

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,9 @@ function setEventId(that) {
   return eventId
 }
 
-
 $(document).ready(function () {
   $(".resultsDelete").click(function () {
+    debugger;
     setEventId($(this));
     $.ajax({
       type: "DELETE",
@@ -39,7 +39,7 @@ $(document).ready(function () {
     // debugger;
     $.ajax({
       type: "GET",
-      url: "./api/add/events",
+      url: "./api/search/events",
       data: {
         q: $("#ename").val(),
         timeMin: (new Date(($("#sedate").val() + ' ' + $("#stime").val())).toISOString()),
@@ -49,10 +49,67 @@ $(document).ready(function () {
       success: function (msg) {
         console.log("GET worked")
         console.log($template);
-        // var compiledTemplate = ejs.compile($template)
         var resultsSection = ejs.render($template, msg)
         $('.resultsSection').append(resultsSection);
       }
     })
   })
+  var $addTemplate;
+  $.ajax({
+    type: "GET",
+    url: "/templates/results.ejs",
+    success: function(temp){
+      $addTemplate = temp;
+    }
+  })
+  $("#addEventForm").submit(function(e){
+    e.preventDefault();
+    
+    $.ajax({
+      type: "POST",
+      url: "./api/add/addEvents",
+      data: {
+        "start": (new Date(($("#sedate").val() + ' ' + $("#stime").val())).toISOString()),
+        "end": (new Date($("#edate").val() + ' ' + $("#etime").val())).toISOString(),
+        'location': $("#eplace").val(),
+        'summary': $("#ename").val(),
+        "status": "confirmed",
+        "description": $("#edesc").val(),
+        "colorId": 1
+      },
+      success: function (msg) {
+        console.log("GET worked")
+        document.getElementById("addEventForm").reset();
+      }
+    })
+  })
+
+  var $checkTemplate;
+  $.ajax({
+    type: "GET",
+    url: "/templates/checkFreeTemp.ejs",
+    success: function(temp){
+      $checkTemplate = temp;
+    }
+  })
+$("#checkForm").submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+    type: "GET",
+    url: '/api/check/events',
+    data: {
+      "timeMin" : (new Date(($("#sedate").val() + ' ' + $("#stime").val())).toISOString()),
+      "timeMax" : (new Date($("#edate").val() + ' ' + $("#etime").val())).toISOString(),
+      "timeZone" : (-(new Date().getTimezoneOffset() / 60)).toString().split("").join("0") + ':00',
+    },
+    success: function (msg) {
+      console.log("Check worked")
+      console.log($checkTemplate);
+      var resultsSection = ejs.render($checkTemplate, msg)
+      $('.resultsSection').append(resultsSection);
+      document.getElementById("checkForm").reset();
+    }
+  })
+})
+
 });
--- routes/index.js
+++ routes/index.js
@@ -9,39 +9,62 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/calendarPg.ejs', function (req, res) {
-    res.render('calendarPg', { title: 'Add Event' })
+    res.render('calendarPg', { title: 'Add Event', results: '' })
 })
-
-
-router.post('/calendarPg.ejs', function (req, res) {
-    console.log("req.body.ename", req.body.ename);
-    res.render('calendarPg', { title: 'Something posted' })
+router.post('/api/add/addEvents', function(req, res) {
+    console.log("/api/add/addEvents", req.body)
+    var start = new Date(req.body.start);
+    var end = new Date(req.body.end);
     cal.Events.insert('primary', {
-        'start': { 'dateTime': req.body.sedate + 'T' + req.body.stime + ':00-05:00' },
-        'end': { 'dateTime': req.body.edate + 'T' + req.body.etime + ':00-05:00' },
-        'location': req.body.eplace,
-        'summary': req.body.ename,
-        'status': 'confirmed',
-        'description': req.body.edesc,
-        'colorId': 1
+        'start': {"dateTime": start},
+        'end': {"dateTime": end},
+        'location': req.body.location,
+        'summary': req.body.summary,
+        'status': req.body.status,
+        'description': req.body.description,
+        'colorId': req.body.colorId
     })
         .then(resp => {
             console.log('inserted event:');
             console.log(resp);
+            res.json(resp);
         })
         .catch(err => {
-            console.log('Error: insertEvent-' + err.message);
+            console.log('Error: insertEvent-/api/add/addEvents' + err.message);
         });
 })
 
+router.post('/calendarPg.ejs', function (req, res) {
+    console.log("req.body.ename", req.query.summary);
+
+    cal.Events.insert('primary',  {
+        'start': req.query.start,
+        'end': req.query.end,
+        'location': req.query.location,
+        'summary': req.query.summary,
+        'status': req.query.status,
+        'description': req.query.description,
+        'colorId': req.query.colorId
+    })
+        .then(resp => {
+            console.log('inserted event:');
+            console.log(resp);
+            console.log(resp.start.dateTime);
+            res.render('calendarPg', { title: 'Something posted'})
+        })
+        .catch(err => {
+            console.log('Error: insertEvent-Other' + err.message);
+        });
+})
+
+
+// SEARCH EVENTS
 router.get('/search', function (req, res, next) {
     res.render('searchEvents.ejs', { title: 'Search Events', results: '' });
 });
-// router.get('/searchEvents.ejs', function(req,res,next) {
-//     res.render('searchEvents.ejs', {title: 'Search Events', results: ''})
-// })
-router.get('/api/add/events',function(req,res){
-    console.log("/api/add/events", req.query)
+
+router.get('/api/search/events/',function(req,res){
+    console.log("/api/search/events", req.query)
     cal.Events.list('primary', {"q": req.query.q, "timeMin": req.query.timeMin, "timeMax": req.query.timeMax})
         .then(json => {
             //Success
@@ -93,54 +116,83 @@ router.delete('/searchEvents.ejs', function (req, res) {
     res.render('calendarPg', { title: "Something Deleted" })
 })
 
+
+
+// CHECK EVENTS
+
+
 router.get('/checkFree', function (req, res, next) {
     res.render('checkFree.ejs', { title: 'Check Availability', results: '' });
 });
-// router.get('/checkFree', function(req,res,next) {
-//     res.render('checkFree.ejs', {title: 'Check Availability', results: ''});
-// })
 
-
-router.get('/checkFree.ejs', function (req, res) {
+router.get('/api/check/events/',function(req,res){
+    console.log("/api/check/events/")
     var response = {
-        "timeMin": req.query.sedate + 'T' + req.query.stime + ':00-05:00',
-        "timeMax": req.query.edate + 'T' + req.query.etime + ':00-05:00',
-        'timeZone': (-(new Date().getTimezoneOffset() / 60)).toString().split("").join("0") + ':00',
+        "timeMin": req.query.timeMin,
+        "timeMax": req.query.timeMax,
+        'timeZone': req.query.timeZone,
         "items": [{ "id": 'primary' }]
     };
     cal.FreeBusy.query('primary', response)
         .then(resp => {
             console.log('List of busy timings with events within defined time range: ');
-            console.log("stringify",JSON.stringify(resp));
-            res.render("checkFree", { title: "Check Availability", results: resp })
-            res.end(JSON.stringify(resp));
+            res.json( { results: resp })
+            // res.end(JSON.stringify(resp));
         })
         .catch(err => {
+            // res.render("checkFree", {title: "Check Availability", results: ''})
             console.log('Error: checkBusy -' + err.message);
         });
-    console.log("response", response)
-    // res.render('checkFree.ejs', { title: 'Check Availability', results: busyTimes })
-    //res.end(JSON.stringify(resp));
-
+    
 })
-
-//Uncomment this to get it to work unRESTfully
 router.get('/checkFree.ejs', function (req, res) {
-    console.log("Checking Availability")
+    console.log("Is this running")
+    var response = {
+        "timeMin": req.query.timeMin,
+        "timeMax": req.query.timeMax,
+        'timeZone': req.query.timeZone,
+        "items": [{ "id": 'primary' }]
+    };
+    // if (response.timeMin != null) {
     cal.FreeBusy.query('primary', {
-        "timeMin": req.query.sedate + 'T' + req.query.stime + ':00-05:00',
-        "timeMax": req.query.edate + 'T' + req.query.etime + ':00-05:00',
-        'timeZone': (-(new Date().getTimezoneOffset() / 60)).toString().split("").join("0") + ':00',
+        "timeMin": req.query.timeMin,
+        "timeMax": req.query.timeMax,
+        'timeZone': req.query.timeZone,
         "items": [{ "id": 'primary' }]
     })
         .then(resp => {
             console.log('List of busy timings with events within defined time range: ');
-            console.log(resp);
-            res.render("checkFree", { title: "Check Availability", results: (resp ? resp : '') })
+            console.log("Successfull FreeBusy Call");
+            res.render("checkFree", { title: "Check Availability", results: resp })
+            // res.end(JSON.stringify(resp));
         })
         .catch(err => {
             console.log('Error: checkBusy -' + err.message);
         });
+    // } else {
+    //     res.render("checkFree.ejs", {title: "Nothing searched", results: ''})
+    // }
+    console.log("response", response)
 
 })
+
+//Uncomment this to get it to work unRESTfully
+// router.get('/checkFree.ejs', function (req, res) {
+//     console.log("Checking Availability")
+//     cal.FreeBusy.query('primary', {
+//         "timeMin": req.query.sedate + 'T' + req.query.stime + ':00-05:00',
+//         "timeMax": req.query.edate + 'T' + req.query.etime + ':00-05:00',
+//         'timeZone': (-(new Date().getTimezoneOffset() / 60)).toString().split("").join("0") + ':00',
+//         "items": [{ "id": 'primary' }]
+//     })
+//         .then(resp => {
+//             console.log('List of busy timings with events within defined time range: ');
+//             console.log(resp);
+//             res.render("checkFree", { title: "Check Availability", results: (resp ? resp : '') })
+//         })
+//         .catch(err => {
+//             console.log('Error: checkBusy -' + err.message);
+//         });
+
+// })
 module.exports = router;
