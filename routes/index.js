7169e0dbc61d263a3e7ef944a9fd9b3b747ae7e7
wcalvert88 <wcalvert88@gmail.com>
2018-03-06 10:43:55 -0500

    Removed un-needed file and cleaned up the router and minor modifications to the ejs files

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,38 @@ var router = express.Router();
 const CONFIG = require('../config/Settings/settings');
 const CalendarAPI = require('node-google-calendar');
 let cal = new CalendarAPI(CONFIG);
-/* GET home page. */
+
+// Main Page Add Events
+// This sets the main page path to Adding an event
 router.get('/', function (req, res, next) {
+    // This renders the calendarPg.ejs file to run on the main page.
     res.render('calendarPg.ejs', { title: 'Add Event' });
 });
 
-router.get('/calendarPg.ejs', function (req, res) {
-    res.render('calendarPg', { title: 'Add Event', results: '' })
+/*This sets the post method for the main page.
+It receives the data from the ajax request in main.js.
+It inserts the data into the node-google-calendar function
+to insert events into your calendar*/
+router.post('/', function (req, res) {
+    cal.Events.insert('primary',  {
+        'start': req.query.start,
+        'end': req.query.end,
+        'location': req.query.location,
+        'summary': req.query.summary,
+        'status': req.query.status,
+        'description': req.query.description,
+        'colorId': req.query.colorId
+    })
+        .then(resp => {
+            console.log('inserted event:');
+            res.render('calendarPg', { title: 'Something posted'})
+        })
+        .catch(err => {
+            console.log('Error: insertEvent-Other' + err.message);
+        });
 })
+
+// Not sure about this file I intend to come back later to comment this.
 router.post('/api/add/addEvents', function(req, res) {
     console.log("/api/add/addEvents", req.body)
     var start = new Date(req.body.start);
@@ -34,77 +58,25 @@ router.post('/api/add/addEvents', function(req, res) {
         });
 })
 
-router.post('/calendarPg.ejs', function (req, res) {
-    console.log("req.body.ename", req.query.summary);
-
-    cal.Events.insert('primary',  {
-        'start': req.query.start,
-        'end': req.query.end,
-        'location': req.query.location,
-        'summary': req.query.summary,
-        'status': req.query.status,
-        'description': req.query.description,
-        'colorId': req.query.colorId
-    })
-        .then(resp => {
-            console.log('inserted event:');
-            console.log(resp);
-            console.log(resp.start.dateTime);
-            res.render('calendarPg', { title: 'Something posted'})
-        })
-        .catch(err => {
-            console.log('Error: insertEvent-Other' + err.message);
-        });
-})
-
-
 // SEARCH EVENTS
-router.get('/search', function (req, res, next) {
-    res.render('searchEvents.ejs', { title: 'Search Events', results: '' });
-});
-
-router.get('/api/search/events/',function(req,res){
-    console.log("/api/search/events", req.query)
-    cal.Events.list('primary', {"q": req.query.q, "timeMin": req.query.timeMin, "timeMax": req.query.timeMax})
-        .then(json => {
-            //Success
-            console.log('List of events on calendar within time-range:');
-            console.log(json);
-            res.json( { results: json })
-            //res.json(json);
-        }).catch(err => {
-            //Error
-            console.log('Error: listSingleEvents -' + err.message);
-        });
-})
-
-router.get('/searchEvents.ejs', function (req, res) {
-    console.log("req", req.query)
-    console.log("req.query.sedate", req.query.sedate)
-    console.log("req.query.edate", req.query.edate)
-    console.log("req.query.stime", req.query.stime),
-        console.log("req.query.etime", req.query.etime)
-    console.log("req.query.ename", req.query.ename)
+// This sets the /search route for the site and renders it
+// with the searchEvents.ejs file
+router.get('/search', function (req, res) {
     cal.Events.list('primary', {"q": req.query.q, "timeMin": req.query.timeMin, "timeMax": req.query.timeMax}
     )
         .then(json => {
             //Success
             console.log('List of events on calendar within time-range:');
-            console.log(json);
             res.render('searchEvents', { title: 'Something Searched', results: json })
-            //res.json(json);
         }).catch(err => {
             //Error
             console.log('Error: listSingleEvents -' + err.message);
+            res.render('searchEvents', {title: "Search Events", results: ''})
         });
 })
 
-router.post('/searchEvents.ejs', function (req, res) {
-    console.log("req.body.ename", req.body.ename);
-    res.render('calendarPg', { title: 'Something Searched' })
-
-})
-
+// This deletes an event from the calendar
+// This does not refresh the page but it does work
 router.delete('/searchEvents.ejs', function (req, res) {
     console.log("Deleting", req.body.eventId);
     cal.Events.delete('primary', req.body.eventId, { sendNotifications: false })
@@ -113,18 +85,48 @@ router.delete('/searchEvents.ejs', function (req, res) {
         }).catch(err => {
             console.log('Error deleteEvent:' + JSON.stringify(err.message));
         });
-    res.render('calendarPg', { title: "Something Deleted" })
+})
+
+// This is the page with the JSON data in it.
+// This lists all the search results as JSON data
+router.get('/api/search/events/',function(req,res){
+    cal.Events.list('primary', {"q": req.query.q, "timeMin": req.query.timeMin, "timeMax": req.query.timeMax})
+        .then(json => {
+            //Success
+            console.log('List of events on calendar within time-range:');
+            // This converts the data to JSON and sends it to the page.
+            res.json({ results: json })
+        }).catch(err => {
+            //Error
+            console.log('Error: listSingleEvents -' + err.message);
+        });
 })
 
 
-
 // CHECK EVENTS
-// hello
+// Not sure why var response works in the api/check/events get but not this one.
+// This checks your calendar within a specific time period and returns the times
+// that you are busy in that time period.
+router.get('/checkFree', function (req, res) {
+    cal.FreeBusy.query('primary', {
+        "timeMin": req.query.timeMin,
+        "timeMax": req.query.timeMax,
+        'timeZone': req.query.timeZone,
+        "items": [{ "id": 'primary' }]
+    })
+        .then(resp => {
+            console.log('List of busy timings with events within defined time range: ');
+            res.render("checkFree", { title: "Check Availability", results: resp })
+        })
+        .catch(err => {
+            // If there is nothing in the params then you just loaded the page
+            // Meaning results should be loaded with nothing.
+            console.log('Error: checkBusy -' + err.message);
+            res.render('checkFree.ejs', { title: 'Check Availability', results: '' });
+        });
+})
 
-router.get('/checkFree', function (req, res, next) {
-    res.render('checkFree.ejs', { title: 'Check Availability', results: '' });
-});
-
+// Sends the json data to the api
 router.get('/api/check/events/',function(req,res){
     console.log("/api/check/events/")
     var response = {
@@ -137,64 +139,11 @@ router.get('/api/check/events/',function(req,res){
         .then(resp => {
             console.log('List of busy timings with events within defined time range: ');
             res.json( { results: resp })
-            // res.end(JSON.stringify(resp));
-        })
-        .catch(err => {
-            // res.render("checkFree", {title: "Check Availability", results: ''})
-            console.log('Error: checkBusy -' + err.message);
-        });
-    
-})
-router.get('/checkFree.ejs', function (req, res) {
-    console.log("Is this running")
-    var response = {
-        "timeMin": req.query.timeMin,
-        "timeMax": req.query.timeMax,
-        'timeZone': req.query.timeZone,
-        "items": [{ "id": 'primary' }]
-    };
-    // if (response.timeMin != null) {
-    cal.FreeBusy.query('primary', {
-        "timeMin": req.query.timeMin,
-        "timeMax": req.query.timeMax,
-        'timeZone': req.query.timeZone,
-        "items": [{ "id": 'primary' }]
-    })
-        .then(resp => {
-            console.log('List of busy timings with events within defined time range: ');
-            console.log("Successfull FreeBusy Call");
-            res.render("checkFree", { title: "Check Availability", results: resp })
-            // res.end(JSON.stringify(resp));
         })
         .catch(err => {
             console.log('Error: checkBusy -' + err.message);
-        });
-    // } else {
-    //     res.render("checkFree.ejs", {title: "Nothing searched", results: ''})
-    // }
-    console.log("response", response)
-
+        });  
 })
-
-//Uncomment this to get it to work unRESTfully
-// router.get('/checkFree.ejs', function (req, res) {
-//     console.log("Checking Availability")
-//     cal.FreeBusy.query('primary', {
-//         "timeMin": req.query.sedate + 'T' + req.query.stime + ':00-05:00',
-//         "timeMax": req.query.edate + 'T' + req.query.etime + ':00-05:00',
-//         'timeZone': (-(new Date().getTimezoneOffset() / 60)).toString().split("").join("0") + ':00',
-//         "items": [{ "id": 'primary' }]
-//     })
-//         .then(resp => {
-//             console.log('List of busy timings with events within defined time range: ');
-//             console.log(resp);
-//             res.render("checkFree", { title: "Check Availability", results: (resp ? resp : '') })
-//         })
-//         .catch(err => {
-//             console.log('Error: checkBusy -' + err.message);
-//         });
-
-// })
 
 module.exports = router;
 
